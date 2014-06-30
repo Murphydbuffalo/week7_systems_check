ac8fbc860ad9422f293f55c5144163a3538e8353
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-30 11:21:33 -0400

    Created features directory with cars and manufacturers sub-directories. Created first test, user_creates_a_new_manufacturer with scenarios for valid creation, using non-unique values, and leaving fields blank.

---
+++ spec/features/manufacturers/users_creates_a_new_manufacturer_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../rails_helper'
+
+feature 'User creates a new manufacturer.' do 
+  scenario 'Valid name and country are entered.' do
+    
+  end
+
+  scenario 'Unavailable value is entered.' do
+  end
+
+  scenario 'A field is left blank.' do
+  end
+end
