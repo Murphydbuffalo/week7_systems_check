840acdf0d56423d31303f1df21cb4d772b2d9826
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-30 11:22:14 -0400

    Removed warnings from .rspec.

--- spec/features/manufacturers/users_creates_a_new_manufacturer_spec.rb
+++ spec/features/manufacturers/users_creates_a_new_manufacturer_spec.rb
@@ -2,12 +2,31 @@ require_relative '../../rails_helper'
 
 feature 'User creates a new manufacturer.' do 
   scenario 'Valid name and country are entered.' do
-    
+    visit new_manufacturer_path
+    fill_in 'Name', with: 'Chevy'
+    fill_in 'Country', with: 'USA'
+    click_on 'Add Manufacturer'
+
+    expect(page).to have_content('Manufacturer Added.')
+    expect(Manufacturer.count).to eq(1)
   end
 
   scenario 'Unavailable value is entered.' do
+    chevy = FactoryGirl.create(:manufacturer)
+
+    visit new_manufacturer_path
+    fill_in 'Name', with: 'Chevy'
+    fill_in 'Country', with: 'USA'
+    click_on 'Add Manufacturer'
+
+    expect(page).to have_content('That manufacturer is already in the database.')
   end
 
   scenario 'A field is left blank.' do
+    visit new_manufacturer_path
+    fill_in 'Name', with: 'Chevy'
+    click_on 'Add Manufacturer'
+
+    expect(page).to have_content('Please provide a name and a country.')
   end
 end
